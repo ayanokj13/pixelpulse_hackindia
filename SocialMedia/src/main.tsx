bc2fa608530237cd6d7690bb1580fc73b652049d
Saloninaik12 <155801599+Saloninaik12@users.noreply.github.com>
2025-03-10 22:49:49 +0530

    Update main.tsx

--- SocialMedia/src/main.tsx
+++ SocialMedia/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import JobSearch from "./components/jobsearch";
 import JobListing from "./components/joblistng";
+import RecruiterLog from "./components/recruiterlog";
+
+import RecruiterSignup from "./components/recrutiersignup"; // Import Recruiter Signup
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -11,6 +14,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<JobSearch />} />
         <Route path="/jobs" element={<JobListing />} />
+        <Route path="/recruiterlog" element={<RecruiterLog />} /> {/* Recruiter Login */}
+        <Route path="/recruitersignup" element={<RecruiterSignup />} /> {/* Recruiter Signup */}
       </Routes>
     </Router>
   </React.StrictMode>
